4f6926f2f230db95f37445af9fbc70a22be912c5
Zakiwidodo <muhzakiwidodo@gmail.com>
2025-04-15 16:44:18 +0700

    deklarasi array dengan ukuran

---
+++ ArrayStruct_0104/Array.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+//deklarasi array tanpa ukuran, tetapi wajib mengisikan data
+int nilai[] = {2,5,9,10,34};
+//deklarasi array dengan ukuran dan diberi nilain awal
+int skor [5] = {8,12,18,6};
