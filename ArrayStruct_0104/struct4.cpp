716b5043c4bd6d871792d9d56307fece58259c5f
Zakiwidodo <muhzakiwidodo@gmail.com>
2025-04-15 16:56:33 +0700

    struct4.0

--- ArrayStruct_0104/struct4.cpp
+++ ArrayStruct_0104/struct4.cpp
@@ -24,3 +24,20 @@ int main(){
     cin.ignore();
     cout << "Mahasiswa Nama =";
     getline(cin, mhs[1].nama);
+    cout << "Mahasiswa Alamat Desa =";
+    cin >> mhs[1].alamat.desa;
+    cout << "Mahasiswa Alamat Kota =";
+    cin >> mhs[1].alamat.kota;
+    }
+
+    cout << endl;
+    cout << "Data Mahasiswa" << endl;
+
+    for(int i = 0; i < 2; i++){
+    cout << "Data Mahasiswa ke-" << i+1 << endl;
+    cout << "NIM =" << mhs[1].nim << endl;
+    cout << "Nama =" << mhs[1].nama << endl;
+    cout << "Alamat Desa =" << mhs[1].alamat.desa << endl;
+    cout << "Alamat Kota =" << mhs[1].alamat.kota << endl;
+    }
+}
