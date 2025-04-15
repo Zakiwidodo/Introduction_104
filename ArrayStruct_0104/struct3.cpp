a6c855d8a9f071185bd1602e0cdda9486dfa6f65
Zakiwidodo <muhzakiwidodo@gmail.com>
2025-04-15 16:53:26 +0700

    struct3

---
+++ ArrayStruct_0104/struct3.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+struct mahasiswa{
+    string nim;
+    string nama;
+    string alamat;
+};
+
+int main(){
+    mahasiswa mhs;
+
+#include <iostream>
+using namespace std;
+
+struct detailAlamat{
+    string desa;
+    string kota;
+
+};
+
+struct mahasiswa{
+    string nim;
+    string nama;
+    detailAlamat alamat;
+};
